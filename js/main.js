0129c21e9e9c8b89d06bedd9b5310833044e1947
user <user@users-MacBook-Pro.local>
2023-02-21 14:06:22 -0800

    Added basic function to calculator. Using javascript created eventlistener to added single digit values to a variable. Will recognize only single digits before allowing component to be selected. Equal operator will open once 2 numbers and operators are selected

--- js/main.js
+++ js/main.js
@@ -1,1 +1,58 @@
-console.log('hello world')
+document.addEventListener('DOMContentLoaded', () => {
+  const numberField = document.querySelector('.numberField');
+  const componentField = document.querySelector('.componentField');
+  const answerField = document.querySelector('.answerField');
+  let firstValue = null;
+  let firstValueSel = false;
+  let secondValue = null;
+  let secondValueSel = false;
+  let componentValue = '';
+  let componentSelected = false;
+
+  numberField.addEventListener('click', (e) => {
+    let target = e.target;
+    let numberValue = target.innerHTML;
+    if (target.tagName === 'P' && firstValue === null) {
+      firstValue = parseFloat(numberValue);
+      firstValueSel = true;
+    } else if (firstValueSel === true && componentSelected === true && secondValueSel === false) {
+      secondValue = parseFloat(numberValue);
+      secondValueSel = true;
+    }
+  });
+
+  componentField.addEventListener('click', (e) => {
+    let target = e.target;
+    let sum = 0;
+    if (target.innerText === '+' && componentSelected === false) {
+      componentValue = 'addition';
+      componentSelected = true;
+    } else if (target.innerText === '-' && componentSelected === false) {
+      componentValue = 'subtraction';
+      componentSelected = true;
+    } else if (target.innerText === '/' && componentSelected === false) {
+      componentValue = 'divide';
+      componentSelected = true;
+    } else if (target.innerText === '*' && componentSelected === false) {
+      componentValue = 'multiply';
+      componentSelected = true;
+    }
+    else if (target.className === 'sum' && componentSelected === true) {
+      if (componentValue === 'addition') {
+        sum = firstValue + secondValue;
+        console.log(`The answer is: ${sum}`);
+      } else if (componentValue === 'subtraction') {
+        sum = firstValue - secondValue;
+        console.log(`The answer is: ${sum}`);
+      } else if (componentValue === 'divide') {
+        sum = firstValue / secondValue;
+        console.log(`The answer is: ${sum}`);
+      } else if (componentValue === 'multiply') {
+        sum = firstValue * secondValue;
+        console.log(`The answer is: ${sum}`);
+      }
+    }
+  })
+
+
+})
