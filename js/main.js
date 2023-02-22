d3a2170d04b0e2ce4dc65850f1f520e91488a590
user <user@users-MacBook-Pro.local>
2023-02-22 00:08:40 -0800

    created sumValueFunc to replace value format in component field eventlistener. created eventlistenter to recognize when a number is pressed on keyboard on the 'keyup' event to highlight number and then remove styling. Working on same listener for components to recognize the same, but only recognizes minus and slash components

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const numberField = document.querySelector('.numberField');
   const componentField = document.querySelector('.componentField');
   const calcuDisplay = document.querySelector('.calcuDisp input');
-  calcuDisplay.value = 0;
+  calcuDisplay.value = '';
   let firstValue = '';
   let firstValueSel = false;
   let secondValue = '';
@@ -10,29 +10,85 @@ document.addEventListener('DOMContentLoaded', () => {
   let componentValue = '';
   let componentSelected = false;
 
-  function componentTypeFunc(className) {
-    componentValue = className;
-    calcuDisplay.value = ''
-    componentSelected = true;
-  }
+  function calcuDispTextFunc(value) {
+    calcuDisplay.value = value;
+  };
 
+  function componentSelectFunc(className, type) {
+    componentValue = className;
+    calcuDispTextFunc(type);
+    componentSelected = true;
+  };
+
+  function sumValueFunc(firstValueArg, secondValueArg) {
+    let sum = 0;
+    if (componentValue === 'add') {
+      sum = parseFloat(firstValueArg) + parseFloat(secondValueArg);
+      calcuDispTextFunc(sum);
+    } else if (componentValue === 'minus') {
+      sum = parseFloat(firstValueArg) - parseFloat(secondValueArg);
+      calcuDispTextFunc(sum);
+    } else if (componentValue === 'divide') {
+      sum = parseFloat(firstValueArg) / parseFloat(secondValueArg);
+      calcuDispTextFunc(sum);
+    } else if (componentValue === 'multiply') {
+      sum = parseFloat(firstValueArg) * parseFloat(secondValueArg);
+      calcuDispTextFunc(sum);
+    }
+  };
+
+  //eventListener will register when clicked with mouse
   numberField.addEventListener('click', (e) => {
     let target = e.target;
     let numberValue = target.innerHTML;
     if (target.tagName === 'P' && componentSelected === false) {
       firstValue = firstValue + numberValue;
-      calcuDisplay.value = firstValue;
+      calcuDispTextFunc(firstValue);
       firstValueSel = true;
     } else if (componentSelected === true) {
       secondValue = secondValue + numberValue;
-      calcuDisplay.value = secondValue;
+      calcuDispTextFunc(secondValue);
       secondValueSel = true;
+    }
+  });
+
+  //eventlistener will register when clicked with keyboard
+  calcuDisplay.addEventListener('keyup', (e)=> {
+    let pElements = numberField.querySelectorAll('p');
+    let pComponents = componentField.querySelectorAll('p');
+    
+    for(let i=0; i<pElements.length;i++) {
+      let pElement = pElements[i];
+      let pElementTextCode = `Digit${pElement.innerText}`;
+      if(pElementTextCode === e.code) {
+        pElement.style.backgroundColor = '#f09e3c';
+        setTimeout(() => {
+          pElement.removeAttribute('style');
+        }, 100);
+        break;
+      }
+    }
+    console.log(e.code.toLowerCase());
+    for(let i=0; i<pComponents.length;i++) {
+      let pComponent = pComponents[i];
+      let pComponentCode = pComponent.className;
+      if(pComponentCode === 'add') {
+        pComponentCode = 'shiftright'
+        console.log(pComponentCode)
+      } else if(pComponentCode = 'multiply') {
+        pComponentCode = 'shiftleft'
+      }
+      if(pComponentCode === e.code.toLowerCase()) {
+        pComponent.style.backgroundColor = '#f09e3c'
+        setTimeout(() => {
+          pComponent.removeAttribute('style');
+        }, 110);
+      }
     }
   });
 
   componentField.addEventListener('click', (e) => {
     let target = e.target;
-    let sum = 0;
     if (target.innerText === 'CLR') {
       firstValue = '';
       firstValueSel = false;
@@ -40,33 +96,21 @@ document.addEventListener('DOMContentLoaded', () => {
       secondValueSel = false;
       componentValue = '';
       componentSelected = false;
-      calcuDisplay.value = ''
+      calcuDisplay.value = '';
     }
     if (target.className === 'add' && componentSelected === false) {
-      componentTypeFunc(target.className)
-    } else if (target.className === 'subtract' && componentSelected === false) {
-      componentTypeFunc(target.className)
+      componentSelectFunc(target.className, target.innerText);
+    } else if (target.className === 'minus' && componentSelected === false) {
+      componentSelectFunc(target.className, target.innerText);
     } else if (target.className === 'divide' && componentSelected === false) {
-      componentTypeFunc(target.className)
+      componentSelectFunc(target.className, target.innerText);
     } else if (target.className === 'multiply' && componentSelected === false) {
-      componentTypeFunc(target.className)
+      componentSelectFunc(target.className, target.innerText);
     }
     else if (target.className === 'sum' && componentSelected === true) {
-      if (componentValue === 'add') {
-        sum = parseFloat(firstValue) + parseFloat(secondValue);
-        calcuDisplay.value = sum;
-      } else if (componentValue === 'subtract') {
-        sum = parseFloat(firstValue) - parseFloat(secondValue);
-        calcuDisplay.value = sum;
-      } else if (componentValue === 'divide') {
-        sum = parseFloat(firstValue) / parseFloat(secondValue);
-        calcuDisplay.value = sum;
-      } else if (componentValue === 'multiply') {
-        sum = parseFloat(firstValue) * parseFloat(secondValue);
-        calcuDisplay.value = sum;
-      }
+      sumValueFunc(firstValue, secondValue);
     }
-  })
+  });
 
 
 })
