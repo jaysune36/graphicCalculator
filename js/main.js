b6f23d0319386089d43b668a83a372ce045f32e2
user <user@users-MacBook-Pro.local>
2023-02-22 20:01:06 -0800

    changed calcuDisp target from a <input> to <p>. Changed code in javascript file to account for new change. Changed eventlistener to also listen for new tag

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const numberField = document.querySelector('.numberField');
   const componentField = document.querySelector('.componentField');
-  const calcuDisplay = document.querySelector('.calcuDisp input');
+  const calcuDisplay = document.querySelector('.calcuDisp p');
+  const body = document.querySelector('body');
   calcuDisplay.value = '';
   let firstValue = '';
   let firstValueSel = false;
@@ -11,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let componentSelected = false;
 
   function calcuDispTextFunc(value) {
-    calcuDisplay.value = value;
+    calcuDisplay.innerText = value;
   };
 
   function componentSelectFunc(className, type) {
@@ -53,39 +54,78 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   //eventlistener will register when clicked with keyboard
-  calcuDisplay.addEventListener('keyup', (e)=> {
+  // bodyMain.addEventListener('keyup', (e)=> {
+  //   let pElements = numberField.querySelectorAll('p');
+  //   let pComponents = componentField.querySelectorAll('p');
+
+  //   for(let i=0; i<pElements.length;i++) {
+  //     let pElement = pElements[i];
+  //     let pElementTextCode = `Digit${pElement.innerText}`;
+  //     if(pElementTextCode === e.code) {
+  //       pElement.style.backgroundColor = '#f09e3c';
+  //       setTimeout(() => {
+  //         pElement.removeAttribute('style');
+  //       }, 100);
+  //       break;
+  //     }
+  //   }
+  //   console.log(e.code.toLowerCase());
+  //   for(let i=0; i<pComponents.length;i++) {
+  //     let pComponent = pComponents[i];
+  //     let pComponentCode = pComponent.className;
+  //     if(pComponentCode === 'add') {
+  //       pComponentCode = 'shiftright'
+  //       console.log(pComponentCode)
+  //     } else if(pComponentCode = 'multiply') {
+  //       pComponentCode = 'shiftleft'
+  //     }
+  //     if(pComponentCode === e.code.toLowerCase()) {
+  //       pComponent.style.backgroundColor = '#f09e3c'
+  //       setTimeout(() => {
+  //         pComponent.removeAttribute('style');
+  //       }, 110);
+  //     }
+  //   }
+  // });
+
+  body.addEventListener('keyup', (e) => {
+    // console.log(e);
     let pElements = numberField.querySelectorAll('p');
     let pComponents = componentField.querySelectorAll('p');
-    
-    for(let i=0; i<pElements.length;i++) {
-      let pElement = pElements[i];
-      let pElementTextCode = `Digit${pElement.innerText}`;
-      if(pElementTextCode === e.code) {
-        pElement.style.backgroundColor = '#f09e3c';
-        setTimeout(() => {
-          pElement.removeAttribute('style');
-        }, 100);
-        break;
+    if (componentSelected === false) {
+      for (let i = 0; i < pElements.length; i++) {
+        let pElement = pElements[i];
+        let pElementTextCode = `Digit${pElement.innerText}`;
+        if (pElementTextCode === e.code) {
+          firstValue = firstValue + pElement.innerText;
+          calcuDispTextFunc(firstValue)
+          pElement.style.backgroundColor = '#f09e3c';
+          setTimeout(() => {
+            pElement.removeAttribute('style');
+          }, 100);
+          break;
+        }
       }
     }
-    console.log(e.code.toLowerCase());
-    for(let i=0; i<pComponents.length;i++) {
-      let pComponent = pComponents[i];
-      let pComponentCode = pComponent.className;
-      if(pComponentCode === 'add') {
-        pComponentCode = 'shiftright'
+
+      for (let i = 0; i < pComponents.length; i++) {
+        let pComponent = pComponents[i];
+        let pComponentCode = pComponent.id;
         console.log(pComponentCode)
-      } else if(pComponentCode = 'multiply') {
-        pComponentCode = 'shiftleft'
-      }
-      if(pComponentCode === e.code.toLowerCase()) {
-        pComponent.style.backgroundColor = '#f09e3c'
-        setTimeout(() => {
-          pComponent.removeAttribute('style');
-        }, 110);
-      }
+        if (pComponentCode === e.code) {
+          console.log(true)
+          pComponent.style.backgroundColor = '#f09e3c'
+          setTimeout(() => {
+            pComponent.removeAttribute('style');
+          }, 110);
+          break;
+        }
     }
-  });
+    // console.log(e.code)
+    // if(e.code === 'ShiftRight') {
+    // console.log(true)
+    // }
+  })
 
   componentField.addEventListener('click', (e) => {
     let target = e.target;
@@ -96,7 +136,7 @@ document.addEventListener('DOMContentLoaded', () => {
       secondValueSel = false;
       componentValue = '';
       componentSelected = false;
-      calcuDisplay.value = '';
+      calcuDisplay.innerText = '';
     }
     if (target.className === 'add' && componentSelected === false) {
       componentSelectFunc(target.className, target.innerText);
